55ef9df665f5fe75b3f7b24416668b0056dc7359
Rikki Tooley <hello@rikk.it>
2014-07-25 02:41:40 +0100

    require.js main module, page is now vertically scrolling

---
+++ web/res/js/main.js
@@ -0,0 +1,105 @@
+function greeting(random){
+    var dt = new Date();
+    var hours = dt.getHours();
+
+    var morning = ['Mornin\''];
+    var day = ['Alright?', 'Hi there!'];
+    var evening = ['Evenin\''];
+
+    console.log('it is at least ' + hours + ':00');
+    var greetings;
+    if (hours > 3 && hours < 12) {
+        greetings = morning;
+    }
+    else if (hours > 16 && hours < 21) {
+        greetings = evening;
+    }
+    else {
+        greetings = day;
+    }
+    var index = parseInt(random * greetings.length);
+
+    console.log('Chose greeting  ' + index );
+    return greetings[index];
+}
+
+require(['jquery', 'nprogress', 'jquery.cycle', 'jquery-easing'], function (jQuery, NProgress) {
+    $(window).bind('beforeunload', function() {
+        NProgress.done();
+    });
+
+    function startNProgress() {
+        NProgress.start();
+    }
+
+    var burritoSize;
+    function rewrapBurrito() {
+        const medTileHeight = 134;
+        var testHeight = 0.60 * window.outerHeight;
+        var newHeight = Math.floor(testHeight / medTileHeight) * medTileHeight;
+        newHeight += 70; // account for padding
+
+        if (newHeight > burritoSize) {
+            $('#burrito').css('height', newHeight);
+        }
+    }
+    $(window).resize(rewrapBurrito);
+
+    $(document).ready(function() {
+        burritoSize = $('#burrito').height();
+        rewrapBurrito();
+
+        $('body').css('display', 'block');
+
+        $.get("./api/all", null, function(tiles) {
+
+            var space = $('#burrito');
+            space.children().remove();
+
+            for (var index = 0; index < tiles.length; index++) {
+                (function (i){
+                    var tile = tiles[i];
+                    var animation = getAnimation(tile['animation']);
+
+                    getTemplateAsync(tile['tile-template'], function(template){
+                        var filled = fillTileTemplate(template.clone(), tile.title, tile.href, tile.style);
+
+                        filled.css("order", i.toString());
+                        filled.find('.tile-link').click(startNProgress);
+
+                        if (tile.size)
+                        {
+                            filled.addClass(tile.size);
+                        }
+                        else {
+                            filled.addClass('medium');
+                        }
+
+                        if (tile['content-template'])
+                        {
+                            getTemplateAsync(tile['content-template'], function(template){
+                                var slides = fillContentTemplates(template.clone(), tile.data);
+
+                                var content = filled.find('.tile-content');
+                                content.children().remove();
+
+                                for (var i=0; i < slides.length; i++){
+                                    content.append(slides[i]);
+                                }
+
+                                space.append(filled);
+                                animation(filled, i);
+                            });
+                        }
+                        else {
+                            space.append(filled);
+                            animation(filled, i);
+                        }
+                    });
+                })(index);
+            }
+        });
+
+        $('#header-greeting').text(greeting(Math.random()));
+    });
+});
