c31e52b2238fe24fdb1d181ca841312cfdaa7068
Rikki Tooley <hello@rikk.it>
2014-07-27 21:00:41 +0100

    Updating install script

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
-npm cache clean -f
+forever stop server/index.js
+rm cache.json
+
+git reset --hard master
+git pull
+
+npm cache clean
 npm install -g n
 n stable
 npm install
 jam install
+
+lessc -ru ./web/res/css/style.less > ./web/res/css/style.css
+
+forever start --sourceDir ./server/ index.js 8080
