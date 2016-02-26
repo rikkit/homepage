113eddbf8940d8fe5d3a44dbf3e0e193339c346b
Rikki Tooley <rikki.tooley@thefilter.com>
2016-02-26 19:18:39 +0000

    Moving types into their own files

---
+++ src/homepage.Generator/StringExtensions.cs
@@ -0,0 +1,7 @@
+namespace homepage.Generator
+{
+    public static class StringExtensions
+    {
+        public static string NullIfEmpty(this string s) => string.IsNullOrEmpty(s) ? null : s;
+    }
+}
