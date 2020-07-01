cbe17997e161cf40ae600e2bc08ff16b9eef2566
Rikki Tooley <hello@rikk.it>
2020-07-01 13:59:10 +0100

    Setup Next.js and Strapi

---
+++ src/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5ddfccb4-00c2-474d-aff8-b1db178e3912'
+};
---
+++ src/frontend/lib/api.js
@@ -0,0 +1,138 @@
+async function fetchAPI(query, { variables } = {}) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/graphql`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+
+  const json = await res.json()
+  if (json.errors) {
+    console.error(json.errors)
+    throw new Error('Failed to fetch API')
+  }
+
+  return json.data
+}
+
+export async function getPreviewPostBySlug(slug) {
+  const data = await fetchAPI(
+    `
+  query PostBySlug($where: JSON) {
+    posts(where: $where) {
+      slug
+    }
+  }
+  `,
+    {
+      variables: {
+        where: {
+          slug,
+        },
+      },
+    }
+  )
+  return data?.posts[0]
+}
+
+export async function getAllPostsWithSlug() {
+  const data = fetchAPI(`
+    {
+      posts {
+        slug
+      }
+    }
+  `)
+  return data?.allPosts
+}
+
+export async function getAllPostsForHome(preview) {
+  const data = await fetchAPI(
+    `
+    query Posts($where: JSON){
+      posts(sort: "created_at:desc", where: $where) {
+        title        
+        slug
+        created_at
+        excerpt
+        coverImage {
+          url
+        }
+        author {
+          username
+        }
+      }
+    }
+  `,
+    {
+      variables: {
+        where: {
+          ...(preview ? {} : { status: 'published' }),
+        },
+      },
+    }
+  )
+  return data?.posts
+}
+
+export async function getPostAndMorePosts(slug, preview) {
+  const data = await fetchAPI(
+    `
+  query PostBySlug($where: JSON, $where_ne: JSON) {
+    posts(where: $where) {
+      title        
+      slug
+      created_at
+      excerpt
+      content
+      ogImage: coverImage{
+        url
+      }
+      coverImage {
+        url
+      }
+      author {
+        username
+        avatar {
+          url
+        }
+      }
+    }
+
+    morePosts: posts(sort: "created_at:desc", limit: 2, where: $where_ne) {
+      title
+      slug
+      excerpt
+      created_at
+      coverImage {
+        url
+      }
+      author {
+        username
+        avatar {
+          url
+        }
+      }
+    }
+  }
+  `,
+    {
+      preview,
+      variables: {
+        where: {
+          slug,
+          ...(preview ? {} : { status: 'published' }),
+        },
+        where_ne: {
+          ...(preview ? {} : { status: 'published' }),
+          slug_ne: slug,
+        },
+      },
+    }
+  )
+  return data
+}
